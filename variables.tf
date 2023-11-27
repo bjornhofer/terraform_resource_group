7549d7de408c1c6ec6e3f8bcb7eae689e00d1c5f
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-11-27 14:21:00 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,3 @@
-/*
-variable "subscription_id" {
-  type = string
-}
-
-variable "client_id" {
-  type = string
-}
-
-variable "client_secret" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
-*/
 // Base Information
 variable "resource_group_base_name" {
   description = "Base name for the resource group"

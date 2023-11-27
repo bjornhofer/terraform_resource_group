31f95bb83f27c7f85a40a35bea4307c9fd7492ee
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-11-27 15:26:06 +0100

    fix and extend
    fixed typo and added additional variables

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 locals {
-  resource_group_name = length(var.resource_group_name_suffix) > 0 ? "${var.resource_group_name_prefix}-${var.resource_group_base_name}-${var.resource_group_name_suffix}" : length(var.resource_group_name_prefix) > 0 ? "${var.resource_group_name_prefix}-${var.resource_group_base_name} : var.resource_group_base_name"
+  resource_group_name = length(var.resource_group_name_suffix) > 0 ? "${var.resource_group_name_prefix}${var.resource_group_name_separator}${var.resource_group_base_name}${var.resource_group_name_separator}${var.resource_group_name_suffix}" : length(var.resource_group_name_prefix) > 0 ? "${var.resource_group_name_prefix}${var.resource_group_name_separator}${var.resource_group_base_name}" : var.resource_group_base_name
 }
 
 resource "azurerm_resource_group" "main" {
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "main" {
   location = var.resource_group_location
   tags = var.tags
   lifecycle {
-    ignore_changes = [ tags ]
+    ignore_changes = var.resource_group_ignore_changes
   }
 }
 

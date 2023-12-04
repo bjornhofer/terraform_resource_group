f5178726429c68dcb70a6069789a9835d575474d
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2023-12-04 15:01:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
-
-locals {
-  resource_group_name = length(var.resource_group_name_suffix) > 0 ? "${var.resource_group_name_prefix}${var.resource_group_name_separator}${var.resource_group_base_name}${var.resource_group_name_separator}${var.resource_group_name_suffix}" : length(var.resource_group_name_prefix) > 0 ? "${var.resource_group_name_prefix}${var.resource_group_name_separator}${var.resource_group_base_name}" : var.resource_group_base_name
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  suffix = [ "${var.resource_group_name_suffix}" ]
+  prefix = [ "${var.resource_group_name_prefix}" ]
 }
 
 resource "azurerm_resource_group" "main" {
   count    = var.resource_group_create == true ? 1 : 0
-  name     = local.resource_group_name
+  name     = module.naming.resource_group.name
   location = var.resource_group_location
   tags = var.tags
 }
